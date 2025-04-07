2851f3667e433b028a1adf2d667c03f51c1ac93f
Blacktea1501 <artur.schaefer32@gmail.com>
2025-04-07 12:34:27 +0200

    Add isPrime and nextPrime implementation

--- 01/dictionary/LinkedHashDictionary.java
+++ 01/dictionary/LinkedHashDictionary.java
@@ -2,7 +2,10 @@ package dictionary;
 
 import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
 
+@SuppressWarnings("unchecked")
 public class LinkedHashDictionary<K extends Comparable<? super K>, V> implements Dictionary<K, V>{
 
 //    LinkedHashDictionary verwendet als Implementierung eine Hashtabelle mit linear verketteten
@@ -11,12 +14,55 @@ public class LinkedHashDictionary<K extends Comparable<? super K>, V> implements
 //    neue Größe etwa doppelt so groß und wieder eine Primzahl ist. Die Daten werden dann sofort
 //    umkopiert.
 
-    private Hashtable<K, V> hashtable;
+    LinkedList<Entry<K, V>>[] tab;
+    int size = 0;
 
-    public LinkedHashDictionary(int capacity) {
-        this.hashtable = new Hashtable<>(capacity);
+
+    @Override
+    public V remove(K key) {
+        var it = tab[hash(key)].iterator();
+        while (it.hasNext()){
+            Entry<K,V> e = it.next();
+            if (e.getKey().equals(key)) {
+                it.remove();
+                size--;
+                return e.getValue();
+            }
+        }
+        return null;
     }
 
+    public LinkedHashDictionary(int capacity) {
+        int newCapacity = isPrime(capacity) ? capacity : nextPrime(capacity);
+        this.tab = new LinkedList[newCapacity];
+    }
+
+    private int nextPrime(int n) {
+        if (n <= 2) return 2;
+        if (n % 2 == 0) n++;
+        while (!isPrime(n)) {
+            n += 2;
+        }
+        return n;
+    }
+
+    private boolean isPrime(int n) {
+        if (n < 2) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+
+        int sqrtN = (int) Math.sqrt(n);
+
+        for (int i = 3; i <= sqrtN; i += 2) {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+
+    private int hash(K key) {
+        return Math.abs(key.hashCode()) % tab.length;
+    }
 
 
     @Override
@@ -26,21 +72,48 @@ public class LinkedHashDictionary<K extends Comparable<? super K>, V> implements
 
     @Override
     public V search(K key) {
+        for (Entry<K, V> e : tab[hash(key)]) {
+            if (e.getKey().equals(key)) {
+                return e.getValue();
+            }
+        }
+
         return null;
     }
 
-    @Override
-    public V remove(K key) {
-        return null;
-    }
 
     @Override
     public int size() {
-        return this.hashtable.size();
+        return this.tab.length;
     }
 
     @Override
     public Iterator<Entry<K, V>> iterator() {
-        return null;
+        return new Iterator<>() {
+            private int currentIndex = 0;
+            private Iterator<Entry<K, V>> currentIterator = null;
+
+            @Override
+            public boolean hasNext() {
+                while (currentIterator == null || !currentIterator.hasNext()) {
+                    if (currentIndex >= tab.length) {
+                        return false;
+                    }
+                    if (tab[currentIndex] != null) {
+                        currentIterator = tab[currentIndex].iterator();
+                    }
+                    currentIndex++;
+                }
+                return true;
+            }
+
+            @Override
+            public Entry<K, V> next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return currentIterator.next();
+            }
+        };
     }
 }
